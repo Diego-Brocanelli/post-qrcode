5d35376680dfb8d8b29134151e8747a407e6b348
Diego Brocanelli <diegod2@msn.com>
2017-08-23 20:08:02 -0300

    Commit inicial, contendo a estrutura do projeto.

---
+++ README.md
@@ -0,0 +1,3 @@
+# QR Code - PHP
+
+Estrutura do projeto utilizado para implementação dos recursos no post sobre QR Code com PHP.
---
+++ config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views/',
+));
+
+$app->match('/', function() use($app) {
+    return $app['twig']->render('index.html');
+});
