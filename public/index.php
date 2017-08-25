a349cf68b31002d7b04fb58e39a3716be89e24c6
Diego Brocanelli <diegod2@msn.com>
2017-08-24 21:47:13 -0300

    instalação do bootstrap pelo yarn. Configuração do index para utilizar o servidor embutido do PHP e chamada do arquivo de rota.

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php 
 
-require_once __DIR__.'/../config/bootstrap.php';
+require_once __DIR__.'/../routes/routes.php';
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
 
 $app->run();
